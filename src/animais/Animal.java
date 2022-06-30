a3bcfd1eb08e7cb2c15df39487910b6d2550ae2a
Bel Albuquerque <mariaisabel.albuquerque@mercadolivre.com>
2022-06-30 11:54:55 -0300

    feat: implementa mais atributos e métodos na classe abstrata Animal

--- src/animais/Animal.java
+++ src/animais/Animal.java
@@ -1,7 +1,59 @@
 package animais;
 
 public abstract class Animal {
-public void emitirSom(String som) {
-    System.out.println(som);
-}
+    private String nome;
+    private String som;
+    private boolean fome;
+    private boolean vida;
+
+    public Animal(String nome, String som) {
+        this.nome = nome;
+        this.som = som;
+        this.fome = false;
+        this.vida = true;
+    }
+
+    public boolean isVida() {
+        return vida;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSom() {
+        return som;
+    }
+
+    public boolean isFome() {
+        return fome;
+    }
+
+    public void setVida() {
+        if (!vida) {
+            System.out.println(som);
+            System.out.println(nome + ", morreu...");
+        }
+        this.vida = !vida;
+    }
+
+    public void setFome(boolean fome) {
+        this.fome = fome;
+    }
+
+    public void emitirSom() {
+        System.out.println(this.som);
+    }
+
+    abstract public void comer();
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "nome='" + nome + '\'' +
+                ", som='" + som + '\'' +
+                ", fome=" + fome +
+                ", vida=" + vida +
+                '}';
+    }
 }
