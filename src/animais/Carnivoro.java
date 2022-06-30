8ff8900d22f0eca8fc9ca6b38eef3e3fd6bb0471
Bel Albuquerque <mariaisabel.albuquerque@mercadolivre.com>
2022-06-30 11:59:04 -0300

    feat: implementa interface Carnivoro

---
+++ src/animais/Carnivoro.java
@@ -0,0 +1,29 @@
+package animais;
+
+public interface Carnivoro {
+
+    public default void comerCarne(Animal animal) {
+        if (animal.isFome()) {
+            animal.setFome(false);
+            System.out.println(animal.getSom() + "!");
+            System.out.println(animal.getNome() + ", achou a carne muito boa!");
+        } else {
+            System.out.println(animal.getSom() + "!");
+            System.out.println(animal.getNome() + ", não está com fome!");
+        }
+    }
+
+    public default void comerAnimal(Animal animalCarnivoro, Animal animalVitima) {
+        if (animalVitima.isVida() && animalCarnivoro.isFome()) {
+            System.out.println(animalCarnivoro.getSom() + "!");
+            System.out.println(animalCarnivoro.getNome() + " devorou " + animalVitima.getNome());
+            animalVitima.setVida();
+
+        } else {
+            System.out.println(animalCarnivoro.getSom() + "!");
+            System.out.println(animalCarnivoro.getNome() + ", não está com fome!");
+            System.out.println(animalVitima.getNome() + " disse: Ufa!!!");
+        }
+
+    }
+}
