d5294997c79c4d1a08a8a2d0505cf0ba5675aca3
Bel Albuquerque <mariaisabel.albuquerque@mercadolivre.com>
2022-06-30 12:00:03 -0300

    feat: implementa classe Cachorro

--- src/animais/Cachorro.java
+++ src/animais/Cachorro.java
@@ -1,4 +1,17 @@
 package animais;
 
-public class Cachorro {
+public class Cachorro extends Animal implements Carnivoro {
+    public Cachorro(String nome, String som) {
+        super(nome, som);
+    }
+
+    @Override
+    public void comer() {
+        this.comerCarne(this);
+    }
+
+
+    public void comerAnimal(Animal vitima) {
+        this.comerAnimal(this, vitima);
+    }
 }
